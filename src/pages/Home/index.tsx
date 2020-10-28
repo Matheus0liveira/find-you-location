e14c15a4b009bd3330d6b942b6bc34efbd3e5399
Matheus0liveira <matheusfilho98.ms@gmail.com>
2020-10-28 15:03:21 -0300

    feat: create boxfloat in page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,18 +1,24 @@
 import React from 'react';
+import * as S from './styles';
 import { Map, TileLayer } from 'react-leaflet';
+import Logo from '../../assets/logo.svg';
 
 const Home = () => {
   return (
-    <Map
-      center={[-8.8229483, -44.2194616]}
-      zoom={15}
-      style={{ width: '100%', height: '70%' }}
-    >
-      {/* <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" /> */}
-      <TileLayer
-        url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
-      />
-    </Map>
+    <>
+      <S.Img src={Logo} alt="" />
+      <S.BoxFloat></S.BoxFloat>
+      <Map
+        center={[-8.8229483, -44.2194616]}
+        zoom={15}
+        style={{ width: '100%', height: '70%' }}
+      >
+        {/* <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" /> */}
+        <TileLayer
+          url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
+        />
+      </Map>
+    </>
   );
 };
 
---
+++ src/pages/Home/styles.ts
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+
+export const Img = styled.img`
+  position: absolute;
+  top: 3rem;
+`;
+
+export const BoxFloat = styled.div`
+  position: absolute;
+  top: 18%;
+  z-index: 9999;
+
+  width: 50%;
+  height: 200px;
+  background: #fff;
+
+  border: 2px solid #282a36;
+  border-radius: 2rem;
+  box-shadow: 0px 123px 302px rgba(0, 0, 0, 0.0443409),
+    0px 13.3395px 60.298px rgba(0, 0, 0, 0.0497168),
+    0px 13.0517px 18.6697px rgba(0, 0, 0, 0.3);
+`;
