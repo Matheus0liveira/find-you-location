9006100c8e7632149d498e86346b8a2a941715c0
Matheus0liveira <matheusfilho98.ms@gmail.com>
2020-10-28 11:00:43 -0300

    feat: create page home and added routes

---
+++ src/routes.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { Route, Switch, BrowserRouter } from 'react-router-dom';
+import Home from './pages/Home';
+
+const Router = () => {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={Home} />
+      </Switch>
+    </BrowserRouter>
+  );
+};
+export default Router;
